0bdd0a2f5c7cb8f35f1b6199fd43d61acc1cfc50
jjw1270 <jjw1270@naver.com>
2021-07-09 16:17:33 +0900

    First

---
+++ slave_test/slave_test.ino
@@ -0,0 +1,29 @@
+/*   ~ Simple Arduino - xBee Receiver sketch ~
+  Read an PWM value from Arduino Transmitter to fade an LED
+  The receiving message starts with '<' and closes with '>' symbol.
+    Dev: Michalis Vasilakis // Date:2/3/2016 // Info: www.ardumotive.com // Licence: CC BY-NC-SA                    */
+//Constants
+const int ledPin = 13;
+//Variables
+
+void setup() {  //Start the serial communication
+  Serial.begin(9600); //Baud rate must be the same as is on xBee module
+  pinMode(ledPin, OUTPUT);
+}
+void loop() {
+  
+  if (Serial.available()){
+    //Read the incoming byte
+    char IN = Serial.read();
+    Serial.write(IN);
+    //Start the message when the '<' symbol is received
+    if(IN == 'A')
+    {
+      digitalWrite(ledPin, HIGH);
+    }
+    if(IN == 'B')
+    {
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
